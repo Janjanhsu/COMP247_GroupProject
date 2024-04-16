8b2f45f35af6ed9b4a424659e45c77515665ff19
winghk00 <146040912+winghk00@users.noreply.github.com>
2024-04-16 16:20:50 -0400

    updated interface of Flask

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import numpy as np
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
 
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 @app.route("/")
-def home():
+
+def hello():
     return render_template('index.html')
+
+@app.route("/sel_models", methods=['GET'])
+def step2_sel_models():
+    if 'form' in session:
+        form_data = session['form']
+    if 'pred_results' in session:
+        pred_results_str = session['pred_results']
+        pred_results = eval(pred_results_str)
+        print('yess')
+    else:
+        pred_results = {}
+        print('yess nonon')
+    # Model selection based on user input
+    model_selection = request.args['modelSelection']
+    if (model_selection==None):
+        print('999')
+    model_path = f"{model_selection}.pkl"
+    model = joblib.load(model_path)
+    # Prepare the data for prediction
+    features = pd.DataFrame([form_data])
+    print(form_data)
+    # Predict using the loaded model
+    features = pd.DataFrame([form_data])  
+    pipeline = joblib.load('full_pipeline.pkl')
+    features_prepared = pipeline.transform(features)
+    prediction = model.predict(features_prepared)
+    outcome = 'Fatal' if prediction[0] == 1 else 'Non-Fatal'
+    print('prediction ', prediction)
+    
+    pred_results[model_selection] = prediction[0]
+    print('pred_results ', pred_results)
+    pred_results_str = str(pred_results)
+    session['pred_results'] = pred_results_str
+    return render_template('/models.html', predictions=pred_results)
+    #return redirect("/sel_models", code=302)
 
 @app.route("/result", methods=['POST'])
 def result():
@@ -30,23 +69,16 @@ def result():
             'LONGITUDE': float(request.form['longitude']),
             'INVAGE': float(request.form.get('invage', 0))  
         }
-
-        # Model selection based on user input
-        model_selection = request.form['modelSelection']
-        model_path = f"{model_selection}.pkl"  
-        model = joblib.load(model_path)
-
-        # Prepare the data for prediction
-        features = pd.DataFrame([form_data])  
-        pipeline = joblib.load('full_pipeline.pkl')
-        features_prepared = pipeline.transform(features)
-        # Predict using the loaded model
-        prediction = model.predict(features_prepared)
-        outcome = 'Fatal' if prediction[0] == 1 else 'Non-Fatal'
         
+        #set session
+        session['form'] = form_data
+        session['pred_results'] = {}
+        session.pop("pred_results", None)
         # Render result template with prediction
-        return render_template('result.html', prediction=outcome)
+        return render_template('models.html', predictions={})
     except Exception as e:
+        print('error')
+        print(e)
         return render_template('error.html', error=str(e))  
 
 if __name__ == "__main__":
